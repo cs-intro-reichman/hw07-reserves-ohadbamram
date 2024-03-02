75d66a56d9801d5356bcb69e2de4151cfa457b6d
ohadbamram <ohad.benamram@post.runi.ac.il>
2024-03-02 23:25:41 +0200

    first try

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -5,7 +5,7 @@ public class HashTagTokenizer {
 	public static void main(String[] args) {
 
 		String hashTag = args[0];
-		String []dictionary = readDictionary("dictionary.txt");
+		String[] dictionary = readDictionary("dictionary.txt");
 		breakHashTag(hashTag, dictionary);
 	}
 
@@ -14,26 +14,36 @@ public class HashTagTokenizer {
 
 		In in = new In(fileName);
 
-		// Your code here
+		for(int i = 0; i < dictionary.length; i++){ //inputs each line of the dictionary in each index for the array
+			dictionary[i] = in.readLine();
+		}
 
 		return dictionary;
 	}
-
-	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+	//checks if a certain word is a part of the dictionary
+	public static boolean existInDictionary(String word, String[] dictionary) {
+		for(int i = 0; i < dictionary.length; i++){
+			if (word.equals(dictionary[i])) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
-
+		hashtag = hashtag.toLowerCase(); //lower cases the string
 		// Base case: do nothing (return) if hashtag is an empty string.
         if (hashtag.isEmpty()) {
             return;
         }
  
         int N = hashtag.length();
-
         for (int i = 1; i <= N; i++) {
-		
+				if (existInDictionary(hashtag.substring(0, i), dictionary) == true) {//cheks if the prefix is in the dictionary
+					System.out.println(hashtag.substring(0, i)); //prints if it
+					breakHashTag(hashtag.substring(i), dictionary); // checks the next prefix without the word
+					break;
+				}
         }
     }
 
--- SpellChecker.java
+++ SpellChecker.java
@@ -9,13 +9,23 @@ public class SpellChecker {
 		String correction = spellChecker(word, threshold, dictionary);
 		System.out.println(correction);
 	}
-
+	//returns a string without it's first letter
 	public static String tail(String str) {
-		// Your code goes here
+		if (str.length() == 1) { //if the string has 1 letter, returns an empty string
+			return "";
+		}else{
+			return str.substring(1);
+		}
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		if (word1.length() == 0) return word2.length(); //base case 1
+		if (word2.length() == 0) return word1.length(); //base case 2			
+		if (word1.charAt(0) == word2.charAt(0)) return levenshtein(tail(word1), tail(word2)); //if they have the same head, continue with their tails
+		//else, return the one change (not the same head) + the minimum number of changes in all tail variations
+		return 1 + Math.min(levenshtein(tail(word1), word2), Math.min(levenshtein(word1, tail(word2)),levenshtein(tail(word1),tail(word2))));
+			
+		
 	}
 
 	public static String[] readDictionary(String fileName) {
@@ -23,13 +33,21 @@ public class SpellChecker {
 
 		In in = new In(fileName);
 
-		// Your code here
+		for(int i = 0; i < dictionary.length; i++){ //inputs each line of the dictionary in each index for the array
+			dictionary[i] = in.readLine();
+		}
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		String value = word;
+		for(int i = 0; i < dictionary.length; i++){
+			if (levenshtein(word, dictionary[i]) <= threshold) {
+				value = dictionary[i];
+			}
+		}
+		return value;
 	}
 
 }
